504975e3e9f5d40e8a7f46c707f9c4bee5b2ea95
Ivan Trubach <mr.trubach@icloud.com>
2021-05-19 09:54:06 +0300

    fix: add missing semicolon

--- hosts/bootstrap/configuration.nix
+++ hosts/bootstrap/configuration.nix
@@ -23,7 +23,7 @@
     LoginGraceTime 15s
     RekeyLimit default 30m
     HostKeyAlgorithms ssh-ed25519
-  ''
+  '';
 
   users.users.nixos.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAiAKU7x1o6NPI/7AqwCaC8edvl80//2LgyVSV/3tIfb tie@xhyve"
