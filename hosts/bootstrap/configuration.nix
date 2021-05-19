9f0c069e42547e5c74c093980f7832e05e2e4611
Ivan Trubach <mr.trubach@icloud.com>
2021-05-19 09:31:55 +0300

    feat(bootstrap): add avahi to bootstrap image

--- hosts/bootstrap/configuration.nix
+++ hosts/bootstrap/configuration.nix
@@ -1,8 +1,32 @@
 { config, lib, pkgs, ... }: {
   isoImage.isoName = lib.mkForce "nixos-bootstrap.iso";
 
-  systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
-  users.users.root.openssh.authorizedKeys.keys = [
+  networking.hostName = "nixos-bootstrap";
+
+  # Enable mDNS discovery.
+  services.avahi.enable = true;
+  services.avahi.publish.enable = true;
+  services.avahi.publish.addresses = true;
+
+  # Enable SSH access.
+  services.openssh.enable = true;
+  services.openssh.startWhenNeeded = true;
+  services.openssh.hostKeys = [
+    {
+      path = "/etc/ssh/ssh_host_ed25519_key";
+      type = "ed25519";
+    }
+  ];
+  services.openssh.passwordAuthentication = false;
+  services.openssh.challengeResponseAuthentication = false;
+  services.openssh.extraConfig = ''
+    LoginGraceTime 15s
+    RekeyLimit default 30m
+    HostKeyAlgorithms ssh-ed25519
+  ''
+
+  users.users.nixos.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAiAKU7x1o6NPI/7AqwCaC8edvl80//2LgyVSV/3tIfb tie@xhyve"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFOq52CJ77uZJ7lDpRgODDMaO22PeHi1GB+rRyj7j+o1 tie@goro"
   ];
 }
