b99e82b88fbb2568119651a85cfd5f987511a102
Ivan Trubach <mr.trubach@icloud.com>
2023-03-10 11:50:20 +0300

    feat(pufferpanel): sync with the upstream PR changes

--- overlays/pufferpanel/pufferpanel.nix
+++ overlays/pufferpanel/pufferpanel.nix
@@ -1,5 +1,4 @@
-# Copied from pkgs/servers/pufferpanel/default.nix
-# TODO: submit changes to Nixpkgs upstream.
+# See https://github.com/NixOS/nixpkgs/pull/220407
 
 { lib
 , buildGoModule
@@ -7,6 +6,7 @@
 , makeWrapper
 , stdenv
 , fetchzip
+, fetchpatch
 , pathDeps ? [ ]
 }:
 
@@ -15,10 +15,36 @@ buildGoModule rec {
   version = "2.6.6";
 
   patches = [
-    # Seems to be an anti-feature.
+    # Bump go-sqlite3 version to avoid a GNU C compiler error.
+    # See https://github.com/PufferPanel/PufferPanel/pull/1240
+    (fetchpatch {
+      url = "https://github.com/PufferPanel/PufferPanel/pull/1240/commits/3065dca2d9b05a56789971ccf0f43a7079a390b8.patch";
+      hash = "sha256-ygMrhJoba8swoRBBii7BEiLihqOebLUtSH7os7W3s+k=";
+    })
+
+    # Fix errors in tests.
+    # See https://github.com/PufferPanel/PufferPanel/pull/1241
+    (fetchpatch {
+      url = "https://github.com/PufferPanel/PufferPanel/pull/1241/commits/ffd21bce4bff3040c8e3e783e5b4779222e7a3a5.patch";
+      hash = "sha256-BzGfcWhzRrCHKkAhWf0uvXiiiutWqthn/ed7bN2hR8U=";
+    })
+
+    # Seems to be an anti-feature. Startup is the only place where user/group is
+    # hardcoded and checked.
+    #
+    # There is no technical reason PufferPanel cannot run as a different user,
+    # especially for simple commands like `pufferpanel version`.
     ./disable-group-checks.patch
-    # See https://github.com/mattn/go-sqlite3/issues/803
-    ./bump-sqlite.patch
+
+    # Some tests do not have network requests stubbed :(
+    ./skip-network-tests.patch
+  ];
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X=github.com/pufferpanel/pufferpanel/v2.Hash=none"
+    "-X=github.com/pufferpanel/pufferpanel/v2.Version=${version}-nixpkgs"
   ];
 
   src = fetchFromGitHub {
@@ -45,8 +71,7 @@ buildGoModule rec {
 
   nativeBuildInputs = [ makeWrapper ];
 
-  subPackages = [ "cmd" ];
-  vendorHash = "sha256-jJZ9Vyg+fyNZyAsQNrJTzCaGcxkCn33PWTpUj2syVjg=";
+  vendorHash = "sha256-fB8MxSl9E2W+BdO6i+drbCe9Z3bPHPi0MvpJEomU9co=";
   proxyVendor = true;
 
   postFixup = ''
@@ -54,12 +79,17 @@ buildGoModule rec {
     cp -r ${src}/assets/email $out/share/pufferpanel/templates
     cp -r ${frontend} $out/share/pufferpanel/www
 
-    # Wrap the binary with the path to the external files.
-    mv $out/bin/cmd $out/bin/pufferpanel
-    wrapProgram "$out/bin/pufferpanel" \
-      --set PUFFER_PANEL_EMAIL_TEMPLATES $out/share/pufferpanel/templates/emails.json \
-      --set GIN_MODE release \
-      --set PUFFER_PANEL_WEB_FILES $out/share/pufferpanel/www \
+    # Rename cmd to pufferpanel and remove other binaries.
+    mv $out/bin $TMPDIR/bin
+    mkdir $out/bin
+    mv $TMPDIR/bin/cmd $out/bin/pufferpanel
+
+    # Wrap the binary with the path to the external files, but allow setting
+    # custom paths if needed.
+    wrapProgram $out/bin/pufferpanel \
+      --set-default GIN_MODE release \
+      --set-default PUFFER_PANEL_EMAIL_TEMPLATES $out/share/pufferpanel/templates/emails.json \
+      --set-default PUFFER_PANEL_WEB_FILES $out/share/pufferpanel/www \
       --prefix PATH : ${lib.escapeShellArg (lib.makeBinPath pathDeps)}
   '';
 
