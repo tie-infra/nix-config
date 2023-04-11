80b868e7da4e4e164b90a25b5c0e3020980fa2ff
Ivan Trubach <mr.trubach@icloud.com>
2023-04-11 19:22:13 +0300

    refactor: temporarily disable pufferpanel

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
     # - https://github.com/NixOS/nixpkgs/pull/220506 (update pufferpanel)
     # - https://github.com/NixOS/nixpkgs/pull/225379 (add myself pufferpanel maintainers)
     # - https://github.com/NixOS/nixpkgs/pull/225274 (pufferpanel module)
+    # - https://github.com/NixOS/nixpkgs/pull/205557 (concatLines for pufferpanel module)
     #nixpkgs.url = "nixpkgs/nixos-22.11";
     nixpkgs.url = "github:tie-infra/nixpkgs/nixos-22.11";
     agenix.url = "github:ryantm/agenix";
