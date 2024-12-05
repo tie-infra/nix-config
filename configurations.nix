ae03ecba977c54cf59aca66a5d683f9d7c322f2e
Ivan Trubach <mr.trubach@icloud.com>
2024-12-05 10:34:44 +0300

    chore: remove systemd-socket-proxyd service

--- configurations.nix
+++ configurations.nix
@@ -2,13 +2,8 @@
 {
   flake.nixosConfigurations = {
     bootstrap-x86-64 = nixosWithSystem "x86_64-linux" [ ./hosts/bootstrap/configuration.nix ];
-
     akane = nixosWithSystem "x86_64-linux" [ ./hosts/akane/configuration.nix ];
-    brim = nixosWithSystem "x86_64-linux" [
-      ./hosts/brim/configuration.nix
-      ./hosts/brim/sops.nix
-      ./hosts/brim/systemd-socket-proxyd.nix
-    ];
+    brim = nixosWithSystem "x86_64-linux" [ ./hosts/brim/configuration.nix ];
     kazuma = nixosWithSystem "x86_64-linux" [ ./hosts/kazuma/configuration.nix ];
     saitama = nixosWithSystem "x86_64-linux" [ ./hosts/saitama/configuration.nix ];
     falcon = nixosWithSystem "x86_64-linux" [ ./hosts/falcon/configuration.nix ];
--- hosts/brim/configuration.nix
+++ hosts/brim/configuration.nix
@@ -1,4 +1,15 @@
-{ pkgs, config, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  caddySecrets = [
+    "brim-su-key.pem"
+    "brimworld-online-key.pem"
+  ];
+in
 {
   system.stateVersion = "23.11";
 
@@ -260,6 +271,58 @@
     };
     serviceConfig.LoadCredential = map (
       name: name + ":" + config.sops.secrets."caddy/${name}".path
-    ) config.passthru.caddySecrets;
+    ) caddySecrets;
   };
+
+  sops.templates."minio.env".content = ''
+    MINIO_ROOT_PASSWORD=${config.sops.placeholder."minio/root-password"}
+  '';
+
+  sops.templates."mcactivity.env".content = ''
+    MCACTIVITY_BOT_TOKEN=${config.sops.placeholder."discord/brimworld-bot-token"}
+  '';
+
+  sops.templates."outline.env".content = ''
+    SECRET_KEY=${config.sops.placeholder."outline/secret-key"}
+    UTILS_SECRET=${config.sops.placeholder."outline/utils-secret"}
+    DISCORD_CLIENT_SECRET=${config.sops.placeholder."outline/discord-client-secret"}
+    AWS_SECRET_ACCESS_KEY=${config.sops.placeholder."outline/s3-secret-access-key"}
+  '';
+
+  sops.secrets =
+    lib.listToAttrs (
+      map (secret: {
+        name = "caddy/" + secret;
+        value = {
+          restartUnits = [ "caddy.service" ];
+          sopsFile = ../../secrets/brim.sops.yaml;
+        };
+      }) caddySecrets
+    )
+    // {
+      "outline/secret-key" = {
+        restartUnits = [ "outline.service" ];
+        sopsFile = ../../secrets/brim.sops.yaml;
+      };
+      "outline/utils-secret" = {
+        restartUnits = [ "outline.service" ];
+        sopsFile = ../../secrets/brim.sops.yaml;
+      };
+      "outline/discord-client-secret" = {
+        restartUnits = [ "outline.service" ];
+        sopsFile = ../../secrets/brim.sops.yaml;
+      };
+      "outline/s3-secret-access-key" = {
+        restartUnits = [ "outline.service" ];
+        sopsFile = ../../secrets/brim.sops.yaml;
+      };
+      "discord/brimworld-bot-token" = {
+        restartUnits = [ "mcactivity.service" ];
+        sopsFile = ../../secrets/brim.sops.yaml;
+      };
+      "minio/root-password" = {
+        restartUnits = [ "minio.service" ];
+        sopsFile = ../../secrets/brim.sops.yaml;
+      };
+    };
 }
